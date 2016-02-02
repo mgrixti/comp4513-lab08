4ebc8b938d57b4dc0929b62b722d56fcc063cf2f
Matthew Grixti <matthewpcgrixti@gmail.com>
2016-02-01 21:20:13 -0700

    Exercise 1 done. For real this time

--- js/exercise1.js
+++ js/exercise1.js
@@ -7,7 +7,6 @@ window.onload = function(){
 
     for (i=0; i<divs.length; i++){
         divs[i].addEventListener("click", function(e){
-            console.log(e.target);
             e.target.parentNode.style.transition = "opacity 1s";
             e.target.parentNode.style.opacity = 0;
         });
