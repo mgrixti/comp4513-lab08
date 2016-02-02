9ed69e69cc863a12104ccde78f8c386b99ebb9c8
Matthew Grixti <matthewpcgrixti@gmail.com>
2016-02-01 21:05:15 -0700

    Exercise 1 done

--- js/exercise1.js
+++ js/exercise1.js
@@ -1,2 +1,15 @@
 
 /* add your code for exercise 1 here */
+
+window.onload = function(){
+
+     divs = document.querySelectorAll(".alert");
+
+    for (i=0; i<divs.length; i++){
+        divs[i].addEventListener("click", function(e){
+            console.log(e.target);
+            e.target.parentNode.style.transition = "opacity 1s";
+            e.target.parentNode.style.opacity = 0;
+        });
+    }
+};
